76414d07a0a11562049eeab50cd0c5569c430340
justinkuruvila54 <justinkuruvila54@gmail.com>
2024-02-06 10:00:03 +0530

    commit

---
+++ lib/utils/questions_db.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: camel_case_types
+
+  class Qustions_db {
+  
+      
+      List<Map<String, dynamic>> sportsQuizData = [
+    {
+      "question": "What are volleyball, basketball, and cricket?",
+      "options": ["1. Water sports", "2. Ball sports", "3. Racing sports", "4. Winter sports"],
+      "answer": 1
+    },
+    {
+      "question": "In which sport is a shuttlecock used?",
+      "options": ["1. Volleyball", "2. Badminton", "3. Tennis", "4. Golf"],
+      "answer": 1
+    },
+    {
+      "question": "Which of the following is not a type of cricket format?",
+      "options": ["1. Test", "2. ODI", "3. T20", "4. Slam"],
+      "answer": 3
+    },
+    {
+      "question": "What is the objective of volleyball?",
+      "options": ["1. Score goals", "2. Score runs", "3. Score points by sending  the net", "4. Make baskets"],
+      "answer": 2
+    },
+    {
+      "question": "How many players are there in a standard basketball team?",
+      "options": ["1. 5", "2. 7", "3. 11", "4. 9"],
+      "answer": 0
+    },
+    {
+      "question": "Which country is known for its dominance in cricket?",
+      "options": ["1. USA", "2. Australia", "3. Brazil", "4. Japan"],
+      "answer": 1
+    },
+    {
+      "question": "In basketball, how many points is a three-point shot worth?",
+      "options": ["1. 1 point", "2. 2 points", "3. 3 points", "4. 4 points"],
+      "answer": 2
+    },
+    {
+      "question": "What is the primary objective of cricket?",
+      "options": ["1. Score touchdowns", "2. Score centuries", "3. none", "4. Score runs and take wickets"],
+      "answer": 3
+    },
+    {
+      "question": "Which sport is played with a racquet and a small, rubber ball?",
+      "options": ["1. Basketball", "2. Volleyball", "3. Cricket", "4. Tennis"],
+      "answer": 3
+    },
+    {
+      "question": "In which sport is a sixer scored?",
+      "options": ["1. Volleyball", "2. Cricket", "3. Basketball", "4. Badminton"],
+      "answer": 1
+    },
+    // Add more questions here...
+  ];
+    }
+  
+
---
+++ lib/view/result_screen/result_screen.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: prefer_const_constructors, camel_case_types
+
+import 'package:flutter/material.dart';
+import 'package:quizapp/utils/colorconst.dart';
+import 'package:quizapp/utils/questions_db.dart';
+import 'package:quizapp/view/question_screen/question_screen.dart';
+
+class Result_screen extends StatelessWidget {
+  final int count;
+  //const Result_screen({super.key});
+  Result_screen({required this.count});
+
+  @override
+  Widget build(BuildContext context) {
+    int totalQn = 10;
+    double percentage = (count / totalQn) * 100;
+
+    return Scaffold(
+      backgroundColor: Color_const.mycustomblack,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Congrats!",
+              style: TextStyle(fontSize: 25, color: Color_const.mycustomwhite),
+            ),
+            Text("${percentage.toStringAsFixed(0)}%score",
+                style:
+                    TextStyle(fontSize: 25, color: Color_const.mycustomgreen)),
+            InkWell(
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Question_screen(),
+                    ));
+              },
+              child: Container(
+                height: 60,
+                width: 210,
+                decoration: BoxDecoration(
+                    color: Color_const.mycustomorange,
+                    borderRadius: BorderRadius.circular(30),
+                    border:
+                        Border.all(width: 5, color: Color_const.mycustomwhite)),
+                child: Center(
+                    child: Text(
+                  "Restart",
+                  style:
+                      TextStyle(fontSize: 30, color: Color_const.mycustomwhite),
+                )),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
